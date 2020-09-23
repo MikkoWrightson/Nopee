1d6b1ff26165c0d8edc6c73a7e0b7e6807f6b0d3
Janne Vanhanen <janne.vanhanen@outlook.com>
2020-09-24 00:57:50 +0300

    Camera working

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
+import Webcam from 'react-webcam';
 
 
-const App = ({}) => <p>Hello world!</p>;
+const App = () => {
+  const webcam = React.useRef<Webcam & HTMLVideoElement>(null);
+
+  const capture = React.useCallback(() => {
+    const image = webcam.current?.getScreenshot();
+    console.log(image);
+  }, [webcam]);
+
+  return (
+    <React.Fragment>
+      <button onClick={capture}>Cheese</button>
+      <Webcam ref={webcam} height="100%" width="100%" />
+    </React.Fragment>
+  );
+}
 
 
 export default App;
